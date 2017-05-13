fb4492e472602a85c7c7e41269b3edbcd786def8
cece phu <xzp2@cornell.edu>
2017-05-13 00:08:44 -0400

    data structure of empty spaces for words to fill

--- src/CrosswordGen.java
+++ src/CrosswordGen.java
@@ -1,12 +1,23 @@
 /**
  * Created by cece on 5/4/2017.
  */
+
+import java.io.*;
+
+import javafx.util.Pair;
+import org.json.simple.JSONArray;
+import org.json.JSONException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import javax.swing.*;
 
@@ -62,6 +73,10 @@ public class CrosswordGen {
                     char c = (char)('A' + random.nextInt(26));
                     crossword[x][y] = c;
                 }
+                else {
+                    char c = 0;
+                    crossword[x][y] = 0;
+                }
             }
         }
 
@@ -73,19 +88,169 @@ public class CrosswordGen {
 
 class CrosswordPanel extends JPanel {
     private JTextField textFields[][];
+    private char[][] crossword;
+    private int w;
+    private int h;
+    private ArrayList<ArrayList<Pair>> emptySpaces;
 
     void setCrossword(char array[][]) {
+        crossword = array;
         removeAll();
-        int w = array.length;
-        int h = array[0].length;
+        w = array.length;
+        h = array[0].length;
         setLayout(new GridLayout(w, h));
-        textFields = new JTextField[w][h];
+        emptySpaces = new ArrayList<>();
+
+
+        /*TODO: unify two for loops*/
 
         for (int y=0; y<h; y++) {
-
+            Pair<Integer, Integer> start = new Pair(0,y);
+            int len = 0;
             for (int x=0; x<w; x++) {
-
                 char c = array[x][y];
+                if (c == 0 ) { // reached null
+                    if(len > 1) { // word is longer than 1
+                        Pair<Integer, Integer> end = new Pair(x-1,y);
+                        ArrayList<Pair> toAdd = new ArrayList<Pair>(2);
+                        toAdd.add(start);
+                        toAdd.add(end);
+                        emptySpaces.add(toAdd);
+
+                        // reset
+                        start = new Pair(x+1,y);
+                        len = 0;
+                    }
+                }
+                else if(x == w-1) { // reached end of col
+                    if(len > 1) { // word is longer than 1
+                        Pair<Integer, Integer> end = new Pair(x,y);
+                        ArrayList<Pair> toAdd = new ArrayList<Pair>(2);
+                        toAdd.add(start);
+                        toAdd.add(end);
+                        emptySpaces.add(toAdd);
+
+                        // reset
+                        start = new Pair(x+1,y);
+                        len = 0;
+                    }
+                }
+                else {
+                    len++;
+                }
+            }
+        }
+
+        for (int x=0; x<w; x++) {
+            Pair<Integer, Integer> start = new Pair(x,0);
+            int len = 0;
+            for (int y=0; y<h; y++) {
+                char c = array[x][y];
+                if (c == 0 ) { // reached null
+                    if(len > 1) { // word is longer than 1
+                        Pair<Integer, Integer> end = new Pair(x,y-1);
+                        ArrayList<Pair> toAdd = new ArrayList<Pair>(2);
+                        toAdd.add(start);
+                        toAdd.add(end);
+                        emptySpaces.add(toAdd);
+
+                        // reset
+                        start = new Pair(x,y+1);
+                        len = 0;
+                    }
+                }
+                else if(y == h-1) { // reached end of row
+                    if(len > 1) { // word is longer than 1
+                        Pair<Integer, Integer> end = new Pair(x,y);
+                        ArrayList<Pair> toAdd = new ArrayList<Pair>(2);
+                        toAdd.add(start);
+                        toAdd.add(end);
+                        emptySpaces.add(toAdd);
+
+                        // reset
+                        start = new Pair(x,y+1);
+                        len = 0;
+                    }
+                }
+                else {
+                    len++;
+                }
+            }
+        }
+
+        fillCrossword();
+
+        getParent().validate();
+        repaint();
+    }
+
+    void fillCrossword() {
+        textFields = new JTextField[w][h];
+
+        JSONParser parser = new JSONParser();
+        ArrayList<String> words = new ArrayList<String>();
+
+        try {
+            Object obj = parser.parse(new BufferedReader(new FileReader( "src/json/wordlist.json" )));
+
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray dict = (JSONArray) jsonObject.get("dict");
+            Iterator i = dict.iterator();
+
+            while (i.hasNext()) {
+                JSONObject word = (JSONObject) i.next();
+                words.add(word.get("word").toString());
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("file not found");
+        } catch (IOException ie) {
+            System.out.println("input error");
+        } catch (ParseException pe) {
+            System.out.println("parse error");
+            pe.printStackTrace();
+        }
+
+        for(ArrayList<Pair> arr : emptySpaces) {
+            if(arr.get(0).getKey().equals(arr.get(1).getKey())) { // same x
+                int length = (int) arr.get(1).getValue()+1;
+                int row = (int) arr.get(0).getKey();
+
+                for(String word : words) {
+                    int counter = 0;
+                    if(word.length() == length) { // word fits
+                        for(int i= (int) arr.get(0).getValue(); i< length; i++) {
+                            textFields[row][i] = new JTextField(word.charAt(counter));
+                            textFields[row][i].setFont(textFields[row][i].getFont().deriveFont(20.0f));
+                            add(textFields[row][i]);
+                            counter++;
+                        }
+                    }
+                }
+            }
+            else { // same y
+                int length = (int) arr.get(1).getKey()+1;
+                int col = (int) arr.get(0).getValue();
+
+                for(String word : words) {
+                    int counter = 0;
+                    if(word.length() == length) { // word fits
+                        for(int i= (int) arr.get(0).getKey(); i< (int) arr.get(1).getKey(); i++) {
+                            textFields[i][col] = new JTextField(word.charAt(counter));
+                            textFields[i][col].setFont(textFields[i][col].getFont().deriveFont(20.0f));
+                            add(textFields[i][col]);
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+        }
+
+/*
+        for (int y=0; y<h; y++) {
+            for (int x=0; x<w; x++) {
+                char c = crossword[x][y];
                 if (c != 0) {
                     textFields[x][y] = new JTextField(String.valueOf(c));
                     textFields[x][y].setFont(textFields[x][y].getFont().deriveFont(20.0f));
@@ -95,9 +260,7 @@ class CrosswordPanel extends JPanel {
                     add(new JLabel());
                 }
             }
-        }
-        getParent().validate();
-        repaint();
+        }*/
     }
 
     char[][] getCrossword() {
